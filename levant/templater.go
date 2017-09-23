hashicorp/levant

commit 1ca427e339d9077a72d109974feec3ccbd0c0289

command/deploy: disable templating when no variables are passed
lavent: handle job file read errors for better message

levant/templater.go
@@ -4,14 +4,14 @@ import (
 	"bytes"
 	"fmt"
 	"io/ioutil"
-	"strings"
+	"path"
 	"text/template"
 
-	nomad "github.com/hashicorp/nomad/api"
 	"github.com/jrasell/levant/helper"
 	"github.com/jrasell/levant/logging"
 	yaml "gopkg.in/yaml.v2"
 
+	nomad "github.com/hashicorp/nomad/api"
 	"github.com/hashicorp/nomad/jobspec"
 	"github.com/hashicorp/terraform/config"
 )
@@ -26,21 +26,30 @@ const (
 // passed variables file.
 func RenderTemplate(templateFile, variableFile string, flagVars *map[string]string) (job *nomad.Job, err error) {
 
-	var tpl bytes.Buffer
+	var tpl *bytes.Buffer
+	ext := path.Ext(variableFile)
 
-	s := strings.Split(variableFile, ".")
-	ext := s[len(s)-1]
+	src, err := ioutil.ReadFile(templateFile)
+	if err != nil {
+		return
+	}
 
 	switch ext {
 	case terraformVarExtention:
 		logging.Debug("levant/templater: detected .tf variable file extension")
-		tpl, err = renderTFTemplte(templateFile, variableFile, flagVars)
+		tpl, err = renderTFTemplte(string(src), variableFile, flagVars)
 	case yamlVarExtension, ymlVarExtension:
 		logging.Debug("levant/templater: detected .yaml or .yml variable file extension")
-		tpl, err = renderYAMLVarsTemplate(templateFile, variableFile, flagVars)
+		tpl, err = renderYAMLVarsTemplate(string(src), variableFile, flagVars)
 	case "":
+		if len(*flagVars) == 0 {
+			logging.Debug("levant/template: no variables file or var flags, skipping templating")
+			tpl = bytes.NewBuffer(src)
+			break
+		}
+
 		logging.Debug("levant/templater: variable file not passed, using any passed CLI variables")
-		tpl, err = readJobFile(templateFile, flagVars)
+		tpl, err = readJobFile(string(src), flagVars)
 	default:
 		err = fmt.Errorf("variables file extension %v not supported", ext)
 	}
@@ -49,19 +58,17 @@ func RenderTemplate(templateFile, variableFile string, flagVars *map[string]stri
 		return
 	}
 
-	// Parse the rendered template as a Nomad job.
-	if job, err = jobspec.Parse(&tpl); err != nil {
-		return
-	}
+	job, err = jobspec.Parse(tpl)
 	return
 }
 
-func renderTFTemplte(templateFile, variableFile string, flagVars *map[string]string) (tpl bytes.Buffer, err error) {
+func renderTFTemplte(src, variableFile string, flagVars *map[string]string) (tpl *bytes.Buffer, err error) {
 
 	c := &config.Config{}
 
 	// Setup our variables map and template
 	variables := make(map[string]interface{})
+	tpl = &bytes.Buffer{}
 
 	// Load the variables file
 	if c, err = config.LoadFile(variableFile); err != nil {
@@ -79,21 +86,22 @@ func renderTFTemplte(templateFile, variableFile string, flagVars *map[string]str
 
 	// Setup the template file for rendering
 	t := template.New("jobTemplate")
-	tempF, _ := ioutil.ReadFile(templateFile)
-	if t, err = t.Parse(string(tempF)); err != nil {
+
+	if t, err = t.Parse(src); err != nil {
 		return
 	}
 
-	if err = t.Execute(&tpl, mergedVars); err != nil {
+	if err = t.Execute(tpl, mergedVars); err != nil {
 		return
 	}
 	return tpl, nil
 }
 
-func renderYAMLVarsTemplate(templateFile, variableFile string, flagVars *map[string]string) (tpl bytes.Buffer, err error) {
+func renderYAMLVarsTemplate(src, variableFile string, flagVars *map[string]string) (tpl *bytes.Buffer, err error) {
 
 	// Setup our variables map and template
 	variables := make(map[string]interface{})
+	tpl = &bytes.Buffer{}
 
 	yamlFile, err := ioutil.ReadFile(variableFile)
 	if err != nil {
@@ -110,28 +118,29 @@ func renderYAMLVarsTemplate(templateFile, variableFile string, flagVars *map[str
 
 	// Setup the template file for rendering
 	t := template.New("jobTemplate")
-	tempF, _ := ioutil.ReadFile(templateFile)
+	tempF, _ := ioutil.ReadFile(src)
 	if t, err = t.Parse(string(tempF)); err != nil {
 		return
 	}
 
-	if err = t.Execute(&tpl, mergedVars); err != nil {
+	if err = t.Execute(tpl, mergedVars); err != nil {
 		return
 	}
 
 	return tpl, nil
 }
 
-func readJobFile(templateFile string, flagVars *map[string]string) (tpl bytes.Buffer, err error) {
+func readJobFile(src string, flagVars *map[string]string) (tpl *bytes.Buffer, err error) {
+
+	tpl = &bytes.Buffer{}
 
 	// Setup the template file for rendering
 	t := template.New("jobTemplate")
-	tempF, _ := ioutil.ReadFile(templateFile)
-	if t, err = t.Parse(string(tempF)); err != nil {
+	if t, err = t.Parse(src); err != nil {
 		return
 	}
 
-	if err = t.Execute(&tpl, flagVars); err != nil {
+	if err = t.Execute(tpl, flagVars); err != nil {
 		return
 	}
 

command/deploy.go
@@ -36,7 +36,7 @@ General Options:
 
   -var-file=<file>
     Used in conjunction with the -job-file will deploy a templated job to your
-    Nomad cluster.
+	Nomad cluster.
 `
 	return strings.TrimSpace(helpText)
 }

commit 24e4d716ea2b093d05e892406f65519fb6f7c0ea

Fix issue with file extension parsing and yaml file rendering.
This changes fixes two bugs introduced in 1ca427e33. The first is
that variable files extensions are not being matched due to a
change in the extension discovery which now means the `.` is
included whereas previously it was not.

The second fixes an issue where templates using a yaml/yml file
extension were not being rendered.

Closes #4
Closes #5

levant/templater.go
@@ -17,9 +17,9 @@ import (
 )
 
 const (
-	terraformVarExtention = "tf"
-	yamlVarExtension      = "yaml"
-	ymlVarExtension       = "yml"
+	terraformVarExtention = ".tf"
+	yamlVarExtension      = ".yaml"
+	ymlVarExtension       = ".yml"
 )
 
 // RenderTemplate is the main entry point to render the template based on the
@@ -118,8 +118,7 @@ func renderYAMLVarsTemplate(src, variableFile string, flagVars *map[string]strin
 
 	// Setup the template file for rendering
 	t := template.New("jobTemplate")
-	tempF, _ := ioutil.ReadFile(src)
-	if t, err = t.Parse(string(tempF)); err != nil {
+	if t, err = t.Parse(string(src)); err != nil {
 		return
 	}
 