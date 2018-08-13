c55dbe24f9769efcd98bbdd28ca7f5876569a3a7
James Rasell <jamesrasell@gmail.com>
2018-08-13 13:28:54 +0200

    Prepare for 0.2.3 development effort.

--- version/version.go
+++ version/version.go
@@ -3,12 +3,12 @@ package version
 import "fmt"
 
 // Version is the main version number that is being run at the moment.
-const Version = "0.2.2"
+const Version = "0.2.3"
 
 // VersionPrerelease is a pre-release marker for the version. If this is ""
 // (empty string) then it means that it is a final release. Otherwise, this is
 // a pre-release such as "dev" (in development), "beta", "rc1", etc.
-const VersionPrerelease = ""
+const VersionPrerelease = "dev"
 
 // Get returns a human readable version of Levant.
 func Get() string {
