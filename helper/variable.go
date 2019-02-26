59c126bb03609e0e2d03beeb073ec4ceee474a0d
evanercolano <evan@workmarket.com>
2019-02-25 20:17:59 -0500

    Utilize mergo for variable merge

--- helper/variable.go
+++ helper/variable.go
@@ -1,30 +1,15 @@
 package helper
 
 import (
-	"github.com/rs/zerolog/log"
+	"github.com/imdario/mergo"
 )
 
 // VariableMerge merges the passed file variables with the flag variabes to
 // provide a single set of variables. The flagVars will always prevale over file
 // variables.
 func VariableMerge(fileVars *map[string]interface{}, flagVars *map[string]interface{}) map[string]interface{} {
-
-	out := make(map[string]interface{})
-
-	for k, v := range *flagVars {
-		log.Info().Msgf("helper/variable: using command line variable with key %s and value %s", k, v)
-		out[k] = v
+	if err := mergo.Merge(&*fileVars, *flagVars, mergo.WithOverride); err != nil {
+		panic(err)
 	}
-
-	for k, v := range *fileVars {
-		if _, ok := out[k]; ok {
-			log.Debug().Msgf("helper/variable: variable from file with key %s and value %s overridden by CLI var",
-				k, v)
-			continue
-		}
-		log.Info().Msgf("helper/variable: using variable with key %s and value %v from file", k, v)
-		out[k] = v
-	}
-
-	return out
+	return *fileVars
 }
